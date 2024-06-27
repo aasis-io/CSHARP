a0988d213a5348e3d95cefbfe16d1922b2cec084
Ashish Thapa <heyashishthapa@gmail.com>
2024-06-27 11:50:53 +0545

    my first c# code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            int x, y, a;
+            Console.WriteLine("Enter length of rectangle:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width of rectangle:");
+            y = Convert.ToInt32(Console.ReadLine());
+            a = x * y;
+            Console.WriteLine("Area=" + a.ToString());
+        }
+    }
+    
+}
+
